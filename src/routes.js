b7c7f0b02010a2787ea4b4d389e9d3ef7bbabf01
JOSE HERIBERTO <heriberto.perez@magmalabs.io>
2019-02-22 17:56:54 -0600

    Implements react responsive library in the react_responsive route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,34 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import DefaultLayout from "./DefaultLayout";
 import { Provider } from "react-redux";
+import MediaQuery from 'react-responsive';
+
+const mediaQueriesReactResponsive = () => (
+  <div>
+    <div>Device Test!</div>
+    <MediaQuery query="(min-device-width: 1224px)">
+      <div>You are a desktop or laptop</div>
+      <MediaQuery query="(min-device-width: 1824px)">
+        <div>You also have a huge screen</div>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 1224px)">
+        <div>You are sized like a tablet or mobile phone though</div>
+      </MediaQuery>
+    </MediaQuery>
+    <MediaQuery query="(max-device-width: 1224px)">
+      <div>You are a tablet or mobile phone</div>
+    </MediaQuery>
+    <MediaQuery query="(orientation: portrait)">
+      <div>You are portrait</div>
+    </MediaQuery>
+    <MediaQuery query="(orientation: landscape)">
+      <div>You are landscape</div>
+    </MediaQuery>
+    <MediaQuery query="(min-resolution: 2dppx)">
+      <div>You are retina</div>
+    </MediaQuery>
+  </div>
+);
 
 const NotFound = () => (
   <h1>
@@ -27,6 +55,10 @@ const Home= () => (
   </h1>
 );
 
+const reactingResponsively= () => (
+  mediaQueriesReactResponsive()
+);
+
 export default (store) => {
   return (
     <Provider store={store}>
@@ -35,6 +67,7 @@ export default (store) => {
           <DefaultLayout exact path="/" component={Home} />
           <DefaultLayout exact path="/about" component={About}/>
           <DefaultLayout exact path="/contact" component={Contact}/>
+          <DefaultLayout exact path="/react_responsive" component={reactingResponsively}/>
           <Route path="*" component={NotFound}/>
         </Switch>
       </BrowserRouter>
